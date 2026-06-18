44b40c289940a116563edccb16a9563678dcb3d4
Shreyaoshi Das <sdas200010@gmail.com>
2026-06-18 08:31:49 +0530

    middelware added

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ import express from "express";
 
 const app = express();
 
+app.use((req,res,next)=>{
+  console.log("authentication middleware called");
+  next();
+});
+
+app.use("/firstRoom",(req,res,next)=>{
+  console.log("first middleware called ...");
+  next();
+});
+
+app.get("/firstRoom",(req,res)=>{
+  res.send("this is Room First..")
+});
+
+app.use("/secRoom",(req,res,next)=>{
+  console.log("sec room middleware called ...");
+  next();
+});
+
+app.get("/secRoom",(req,res)=>{
+  res.send("you are in sec room...")
+})
+
 const port = 3000;
 app.listen(port,()=>{
   console.log("server is running on port", port);
